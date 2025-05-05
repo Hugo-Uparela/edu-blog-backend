9066c2c15fd0b1d6b83613775b46792608edac13
Hugo Uparela <hugouparela21@gmail.com>
2025-05-05 01:03:11 -0500

    Formatear el código en el modelo de base de datos para mejorar la legibilidad

--- models/index.js
+++ models/index.js
@@ -1,31 +1,31 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/config');
+const { Sequelize } = require("sequelize");
+const config = require("../config/config");
 
 const sequelize = new Sequelize(config.databaseUrl, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: false,
 });
 
 // Importa los modelos
-const User = require('./User')(sequelize);
-const Question = require('./Question')(sequelize);
-const Answer = require('./Answer')(sequelize);
-const Vote = require('./Vote')(sequelize);
+const User = require("./User")(sequelize);
+const Question = require("./Question")(sequelize);
+const Answer = require("./Answer")(sequelize);
+const Vote = require("./Vote")(sequelize);
 
 // Define asociaciones
-User.hasMany(Question, { foreignKey: 'userId' });
-Question.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Question, { foreignKey: "userId" });
+Question.belongsTo(User, { foreignKey: "userId" });
 
-Question.hasMany(Answer, { foreignKey: 'questionId' });
-Answer.belongsTo(Question, { foreignKey: 'questionId' });
+Question.hasMany(Answer, { foreignKey: "questionId" });
+Answer.belongsTo(Question, { foreignKey: "questionId" });
 
-User.hasMany(Answer, { foreignKey: 'userId' });
-Answer.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Answer, { foreignKey: "userId" });
+Answer.belongsTo(User, { foreignKey: "userId" });
 
-Answer.hasMany(Vote, { foreignKey: 'answerId' });
-Vote.belongsTo(Answer, { foreignKey: 'answerId' });
+Answer.hasMany(Vote, { foreignKey: "answerId" });
+Vote.belongsTo(Answer, { foreignKey: "answerId" });
 
-User.hasMany(Vote, { foreignKey: 'userId' });
-Vote.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Vote, { foreignKey: "userId" });
+Vote.belongsTo(User, { foreignKey: "userId" });
 
 module.exports = { sequelize, User, Question, Answer, Vote };
