d98777f8110f63bfd4bfd46ca74d85a2c5bd35e3
Hugo Uparela <hugouparela21@gmail.com>
2025-05-05 01:02:59 -0500

    Agregar rutas para listar y crear respuestas con autenticación

--- routes/answers.js
+++ routes/answers.js
@@ -1,3 +1,13 @@
+// routes/answers.js
 const express = require('express');
 const router = express.Router();
+const answerController = require('../controllers/answerController');
+const { authenticate } = require('../middleware/auth');
+
+
+router.get('/', answerController.listAnswers);
+
+
+router.post('/', authenticate, answerController.createAnswer);
+
 module.exports = router;
